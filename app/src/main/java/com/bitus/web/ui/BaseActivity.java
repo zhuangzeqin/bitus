76caf316d416f717169c53b7b8717ec48bf120da
zhuangzeqin <zzq@eeepay.cn>
2019-11-28 14:33:16 +0800

    增加实现沉浸式状态栏

--- app/src/main/java/com/bitus/web/ui/BaseActivity.java
+++ app/src/main/java/com/bitus/web/ui/BaseActivity.java
@@ -3,14 +3,17 @@ package com.bitus.web.ui;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.annotation.UiThread;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.Window;
 
 import com.bitus.web.R;
 import com.bitus.web.view.CustomProgressDialog;
+import com.gyf.immersionbar.ImmersionBar;
 
 
 /**
@@ -34,10 +37,18 @@ public abstract class BaseActivity extends AppCompatActivity {
         /** ------不可横屏幕-------- **/
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(getLayoutId());
+        ImmersionBar.with(this).init();
+        //4.4之上才可以修改状态栏颜色，想要设置状态栏为黑字，只有在Android6.0以上系统或者小米的MIUI V6以上版本或者，魅族的Flyme4.0以上版本才可以设置。
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//6.0
+            // View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | (false ? View.SYSTEM_UI_FLAG_LAYOUT_STABLE : View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR));
+        }
+        //自动状态栏字体变色，必须指定状态栏颜色才可以自动变色哦.init();
         initWidget();
         eventOnClick();
-
     }
+
     /**
      * 显示等待对话框
      **/
